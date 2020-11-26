48198a4e6c88a4d3f8706f13809c160960f08bc2
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-26 10:28:59 +0100

    start over excercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-    ]
-  }
-
   render() {
     return (
       <div className="App">
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name="Kristoffer" age="24">My Hobbies: Öl</Person>
+        <h1>Hello World</h1>
       </div>
-    );
+      );
+    }
   }
-}
+
+
 
 
 
