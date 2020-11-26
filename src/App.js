8ad08e683b56b738bfc6b56d2df3caa61baa4113
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-26 13:58:48 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  
+  state = {
+    persons: [
+      {name: "Kristoffer", age: 28},
+      {name: "Eriksson", age: 30}
+    ]
+  }
 
   render() {
     return (
       <div className="App">
-        <Person age="35" name="Kristoffer">My hobby: Photography</Person>
+        <Person age={this.state.persons[0].age} name={this.state.persons[0].name}>My hobby: Photography</Person>
         <br/>
-        <Person age="30" name="Eriksson" />
+        <Person age={this.state.persons[1].age} name={this.state.persons[1].name} />
         <button>Switch Name</button>
       </div>
       );
