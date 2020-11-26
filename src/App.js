2df8d9f8ea84a26137d0cc29b782e0ad9b955c62
Erik Somnäs <eriksomnas@Eriks-iMac.lan>
2020-11-26 13:49:25 +0100

    add elements

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  
+
   render() {
     return (
       <div className="App">
-        <Person age="35"/>
+        <Person age="35" name="Kristoffer">My hobby: Photography</Person>
+        <br/>
+        <Person age="30" name="Eriksson" />
+        <button>Switch Name</button>
       </div>
       );
     }
